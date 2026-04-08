0cc91cb9ceec24e96a0389b4358a20ca6901f592
Kentaro Osakabe <ken_osakabe@bluesinc.jp>
2026-04-08 16:20:21 +0900

    Fix page.tsx query to use coalesce for heroImage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { client } from '../lib/sanity';
 import HomeClient from './HomeClient';
 
 async function getArticles() {
-  const query = '*[_type == "article"] | order(publishedAt desc) { _id, title, "slug": slug.current, pillar, subtitle, "heroImage": heroImage.asset->url, tags, readTime, publishedAt }';
+  const query = '*[_type == "article"] | order(publishedAt desc) { _id, title, "slug": slug.current, pillar, subtitle, "heroImage": coalesce(heroImage.asset->url, heroImageUrl), tags, readTime, publishedAt }';
   return client.fetch(query);
 }
 
